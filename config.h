38b7322d725bac61c8ad67782b7a9ab4c63f74af
dariod-z <97047924+dariod-z@users.noreply.github.com>
2022-01-05 16:16:54 +0100

    Update config.h

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#1a62a5";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#0dcdcd";
